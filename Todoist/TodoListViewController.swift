2b6d53706a07f100af56e761d84ef6a7ffe3c735
Rafael sanchez <management@bionic.it>
2019-03-24 23:28:35 -0400

    222. TableView Delegate Methods and Accessories 7:45
    	1. Create a delegate method that knows which cell was selected
    	2. In the created method - Write a condition where if its accesoryType .checkmark is on then take it off and vice versa.
    	3. In the created method - deselect the cell after its been selected

--- Todoist/TodoListViewController.swift
+++ Todoist/TodoListViewController.swift
@@ -9,9 +9,10 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-    //MARK: -
-    //MARK: Variable Declarations & IBOutlets/IBActions
+ 
     //----------------------------------------------------------------------------------------
+    //MARK: - Variable Declarations & IBOutlets/IBActions
+    
     
     let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
     
@@ -20,41 +21,48 @@ class TodoListViewController: UITableViewController {
     
     
     
-    
+      //----------------------------------------------------------------------------------------
     //MARK: -
     
-    //----------------------------------------------------------------------------------------
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
-    
-    //MARK: Tableview Datasource Methods
+    //----------------------------------------------------------------------------------------
+    //MARK:  - Tableview Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return itemArray.count
     }
     
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
         
-        
         cell.textLabel?.text = itemArray[indexPath.row]
-        
         
         return cell
         
     }
     
+    //MARK: TableView Delegate Methods
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     
-    
-    
-    
+
 }
 
